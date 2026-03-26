91a21c059322d2e2b6168a04405d1166bf7d824b
Abdul Mohaiman <abdulmohaiman333@gmail.com>
2026-03-26 13:47:47 +0500

    Lesson 2

--- Lessons/lesson_2.php
+++ Lessons/lesson_2.php
@@ -40,5 +40,31 @@ $user = [
 
 // 3. Nested Arrays
 
+    // $users = [
+    //     ['name' => 'Ahmad', 'age' => 20, 'id' => 1212],
+    //     ['name' => 'Ali', 'age' => 25, 'id' => 1414],
+    //     ['name' => 'Hamza', 'age' => 23, 'id' => 1313]
+    // ];
+
+    // echo $users[0]['name'];
+    // echo $users[1]['age'];
+    // echo $users[2]['id'];
+
+// Useful Array Functions
+// 1. Counts array values 
+// echo count($fruits)
+// 2. Push item at last
+// array_push($fruits, 'Orange');
+// echo $fruits[3];
+// 3. Check if value exists in the array
+// if( in_array('banana', $fruits)){
+//     echo "Exists";
+// } else{
+//     echo "Not Exists.";
+// }
+// 4. Join two arrays together
+// array_merge($a, $b);
+// 5. Get portion of array (from index 1, take 3 items)
+// array_slice($fruits, 1, 3)
 
 ?>
---
+++ Tasks/task_2.php
@@ -0,0 +1,28 @@
+<?php
+
+    $product = [
+        'name' => 'Stand',
+        'price' => 5000,
+        'category' => 'intensils',
+        'inStock' => 'available'
+    ];
+
+    $product1 = [
+        'name' => 'Stand',
+        'price' => 5000,
+        'category' => 'intensils',
+        'inStock' => 'available'
+    ];
+
+    $product2 = [
+        'name' => 'Stand',
+        'price' => 5000,
+        'category' => 'intensils',
+        'inStock' => 'available'
+    ];
+
+    echo $prodcut1['name'];
+    echo $prodcut1['price'];
+
+
+?>
