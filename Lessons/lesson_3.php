712dd3048a8d083fdaf4e00267537f4696ae9ee8
Abdul Mohaiman <abdulmohaiman333@gmail.com>
2026-03-26 16:36:49 +0500

    Lesson 2

---
+++ Lessons/lesson_3.php
@@ -0,0 +1,14 @@
+<?php
+// If Else statements
+$age = 18;
+
+if($age < 18){
+    echo 'How are you?';
+} elseif($age == 18){
+    echo 'Who are you?';
+} else{
+    echo 'You are good.';
+}
+
+
+?>
---
+++ Lessons/lesson_4.php
@@ -0,0 +1,22 @@
+<?php 
+// FOr Loop
+// $arr = [1,2,3,4,5];
+// for($i=0; $i<5; $i++){
+//     echo "$i<br>";
+// }
+
+// For Each Loop
+
+$products = [
+    ['name' => 'laptop', 'price' => 1000],
+    ['name' => 'phone', 'price' => 2000],
+    ['name' => 'tablet', 'price' => 5000]
+
+];
+
+foreach($products as $product){
+    echo 'Name: '.$product['name'].' Price: '.$product['price'] .'<br>';
+}
+
+
+?>
